4d0d1de56fa8d11ef23a385c04b736cdec3519c1
Michael Toth <tofudrummer@gmail.com>
2016-08-11 12:14:43 -0700

    scraping is easier when the site has classes
    meaningful classes

---
+++ scrim_scrape.js
@@ -0,0 +1,31 @@
+var request = require("request"),
+  cheerio = require("cheerio"),
+  url = "http://www.chairleague.com/scrims";
+
+  
+var scrim_output = request(url, function (error, response, body) {
+  if (!error) {
+    var $ = cheerio.load(body);
+    var scrim = [];
+    // day/month
+    $('.name').each(function(i, name) {
+      scrim[i] = {
+        name: name.children[0].data
+      };
+    });
+    $('.rating').each(function(i, rating) {
+      scrim[i].rating = rating.children[0].data.replace('\n', '')
+    });
+    $('.calendar .month').each(function(i, month) {
+      scrim[i].month = month.children[0].data.replace('\n', '').replace('\n', '')
+    });
+    $('.calendar .day').each(function(i, day) {
+      scrim[i].day = day.children[0].data.replace('\n', '').replace('\n', '')
+    });
+    $('.time').each(function(i, time) {
+      scrim[i].time = time.children[0].data.replace('\n', '') + ' PST'
+    });
+    console.log(scrim);
+    return scrim;
+  }
+});
