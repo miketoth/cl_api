1828c6698285297cda1e1fa480b78b927f007b8d
Michael Toth <tofudrummer@gmail.com>
2016-08-11 10:45:10 -0700

    Actually returns the division object

--- scrape.js
+++ scrape.js
@@ -1,11 +1,12 @@
+var rsvp = require('rsvp');
 var request = require("request"),
   cheerio = require("cheerio"),
   url = "http://www.chairleague.com/divisions/division-1";
 
-var division = [];
-var temp = [];
   
-request(url, function (error, response, body) {
+var output = request(url, function (error, response, body) {
+  var division = [];
+  var temp = [];
   if (!error) {
     var $ = cheerio.load(body);
     var table = $('table')[2].children;
@@ -21,18 +22,15 @@ request(url, function (error, response, body) {
                     if(text.children) {
                       text.children.forEach(function(deepText) {
                         if(text.children[0].data !== undefined && text.children[0].data !== '\n') {
-                          console.log(text.children[0].data);
                           temp.push(text.children[0].data.replace('\n', '').replace('\n', ''));
                         }
                       });
                     }
-                    //console.log(text.data);
                     if(text.data !== undefined && text.data !== '\n') {
                       temp.push(text.data.replace('\n', '').replace('\n',''));
                     }
                   });
                   division.push(temp);
-                  console.log(division);
                 }
               }
             });
@@ -40,8 +38,11 @@ request(url, function (error, response, body) {
         });
       }
     });
+    console.log(division);
+    return division;
       
   } else {
     console.log("We’ve encountered an error: " + error);
   }
 });
+
