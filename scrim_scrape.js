2c80067db7a036a23be73ffb11336a10aab38a8a
Michael Toth <tofudrummer@gmail.com>
2016-09-14 10:03:57 -0700

    Added link to the response
    Modified response to export a node module

--- scrim_scrape.js
+++ scrim_scrape.js
@@ -1,31 +1,38 @@
-var request = require("request"),
-  cheerio = require("cheerio"),
-  url = "http://www.chairleague.com/scrims";
+var chairleague = "http://www.chairleague.com";
+function scrape(callback) {
+  var request = require("request"),
+    cheerio = require("cheerio"),
+    url = "http://www.chairleague.com/scrims";
 
-  
-// TODO: link
-var scrim_output = request(url, function (error, response, body) {
-  if (!error) {
-    var $ = cheerio.load(body);
-    var scrim = [];
-    $('.name').each(function(i, name) {
-      scrim[i] = {
-        name: name.children[0].data
-      };
-    });
-    $('.rating').each(function(i, rating) {
-      scrim[i].rating = rating.children[0].data.replace('\n', '')
-    });
-    $('.calendar .month').each(function(i, month) {
-      scrim[i].month = month.children[0].data.replace('\n', '').replace('\n', '')
-    });
-    $('.calendar .day').each(function(i, day) {
-      scrim[i].day = day.children[0].data.replace('\n', '').replace('\n', '')
-    });
-    $('.time').each(function(i, time) {
-      scrim[i].time = time.children[0].data.replace('\n', '') + ' PST'
-    });
-    console.log(scrim);
-    return scrim;
-  }
-});
+  return request(url, function (error, response, body) {
+    if (!error) {
+      var $ = cheerio.load(body);
+      var scrim = [];
+      $('.name').each(function(i, name) {
+        scrim[i] = {
+          name: name.children[0].data
+        };
+      });
+      $('.rating').each(function(i, rating) {
+        scrim[i].rating = rating.children[0].data.replace('\n', '')
+      });
+      $('.calendar .month').each(function(i, month) {
+        scrim[i].month = month.children[0].data.replace('\n', '').replace('\n', '')
+      });
+      $('.calendar .day').each(function(i, day) {
+        scrim[i].day = day.children[0].data.replace('\n', '').replace('\n', '')
+      });
+      $('.time').each(function(i, time) {
+        scrim[i].time = time.children[0].data.replace('\n', '') + ' PST'
+      });
+      $('.team a').each(function(i, link) {
+        scrim[i].link = chairleague + link.attribs.href;
+      });
+      callback(scrim); // one day this will be a promise. I promise
+    } else {
+      // error handling
+    }
+  });
+}
+
+module.exports = scrape;
